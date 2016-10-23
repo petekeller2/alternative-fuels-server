7398fd9ce46e162cf1916377d4e11d6cd80f4bae
Peter Keller <petekeller2@gmail.com>
2016-10-23 17:11:12 -0400

    radius added

--- server.js
+++ server.js
@@ -9,15 +9,14 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-var nrelAPIkey = process.env.NREL_API_KEY;
-var googleAPIkey = process.env.GOOGLE_API_KEY;
+var nrelAPIkey = process.env.NREL_API_KEY || 'your nrelAPIkey';
+var googleAPIkey = process.env.GOOGLE_API_KEY || 'your googleAPIkey';
 
 app.post('/fuelStationsData', function(req, res) {
     //console.log(req.body);
     //console.log("nrelAPIkey", nrelAPIkey);
-    if(req.body.fuelType && req.body.zipCode) {
-        //was post
-        var nrelUrlString = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?fuel_type=" + req.body.fuelType + "&location=" + req.body.zipCode + "&limit=10&radius=500.0&api_key=" + nrelAPIkey + "&format=JSON";
+    if(req.body.fuelType && req.body.address && req.body.radius) {
+        var nrelUrlString = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?fuel_type=" + req.body.fuelType + "&location=" + req.body.address + "&limit=10&radius=" + req.body.radius + "&api_key=" + nrelAPIkey + "&format=JSON";
         request.get({url:nrelUrlString, json:true}, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 //console.log(body.fuel_stations);
@@ -34,11 +33,11 @@ app.post('/fuelStationsData', function(req, res) {
 
                         if (destinations && origins) {
                             destinations = destinations + "|" + addressString;
-                            origins = origins + "|" + req.body.zipCode;
+                            origins = origins + "|" + req.body.address;
                         }
                         else {
                             destinations = addressString;
-                            origins = req.body.zipCode;
+                            origins = req.body.address;
                         }
                     }
                     console.log(i);
@@ -85,7 +84,7 @@ app.post('/fuelStationsData', function(req, res) {
         });
     }
     else {
-        res.end("zip code and fuel type required!");
+        res.end("address, radius and fuel type required!");
     }
 });
 
