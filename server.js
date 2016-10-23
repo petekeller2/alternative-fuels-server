4eaad41acc8d1f06d6665c31967c8a5513cf94c2
Peter Keller <petekeller2@gmail.com>
2016-10-23 14:17:06 -0400

    cors update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express'),
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://alternative-fuels.firebaseapp.com");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
     next();
